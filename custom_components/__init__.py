4dd964b60ab33af32653967e05cc7830e05a4d01
Tater Totterson <hatted-limited0r@icloud.com>
2025-10-03 08:19:58 -0500

    Add files via upload

---
+++ custom_components/__init__.py
@@ -0,0 +1,9 @@
+from homeassistant.core import HomeAssistant
+
+DOMAIN = "tater_conversation"
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry):
+    return True
