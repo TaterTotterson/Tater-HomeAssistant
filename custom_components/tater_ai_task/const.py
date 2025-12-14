bbabf46d0217931f70552ff0500a5ee668dd53b5
Tater Totterson <hatted-limited0r@icloud.com>
2025-12-14 09:31:31 -0600

    Create const.py

---
+++ custom_components/tater_ai_task/const.py
@@ -0,0 +1,11 @@
+DOMAIN = "tater_ai_task"
+
+CONF_BASE_URL = "base_url"
+CONF_API_KEY = "api_key"
+CONF_TIMEOUT = "timeout"
+CONF_NAME = "name"
+
+DEFAULT_NAME = "Tater"
+DEFAULT_TIMEOUT = 60
+
+PLATFORMS = ["ai_task"]
